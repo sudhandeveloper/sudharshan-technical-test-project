6f0666aeb5d1c877235ecbd3784321f2b1312f9f
sudhandeveloper <gmsudhan3@gmail.com>
2024-02-20 18:46:40 +0530

    new

---
+++ technical test/clientside/index.js
@@ -0,0 +1,14 @@
+// var express = require("express");
+// var cors = require("cors");
+// var app = express();
+// app.use(express.json());
+// app.use(cors());
+
+// app.get("/products", function (req, res, next) {
+//   res.json({ msg: "This is CORS-enabled for all origins!" });
+// });
+
+// app.listen(5000, function () {
+//   console.log("CORS-enabled web server listening on port 5000");
+// });
+// "start": "nodemon index.js",
